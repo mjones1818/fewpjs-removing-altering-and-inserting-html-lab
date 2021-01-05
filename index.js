d517018c2ea7c18bde077c46aef6726c795ee673
mjones1818 <michael.jones0532@gmail.com>
2021-01-05 11:28:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+let element = document.createElement('div')
+document.body.appendChild(element)
+
+let ul = document.createElement('ul')
+
+for (let i = 0; i < 10; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+
+element.appendChild(ul)
+element.style.backgroundColor = '#27647B';
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left'
+ul.removeChild(ul.querySelector('li:nth-child(2)'))
+
+
+document.querySelector('main').remove()
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'is the champion'
